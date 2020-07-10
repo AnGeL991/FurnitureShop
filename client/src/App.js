5e45bcc48ad46039aa18e45503dad67c3e975710
Adrian Markuszewski <adrian991999@wp.pl>
2020-07-10 13:58:43 +0200

    init project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/normalize.scss";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/pages/home/home";
+import PageNoFound from "./components/pages/pageNoFound/pageNoFound";
+import MainLayout from "./components/layout/mainLayout/mainLayout";
+const App = () => {
+  return (
+    <MainLayout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact component={PageNoFound} />
+      </Switch>
+    </MainLayout>
+  );
+};
+
+export default App;
---
+++ client/src/components/pages/pageNoFound/pageNoFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageNoFound =()=>(
+    <div>PageNoFound</div>
+)
+
+export default PageNoFound;
