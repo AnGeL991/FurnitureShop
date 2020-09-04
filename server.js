a17d7b81ac1b6dfef6dac3a18abde1153a93a98f
Adrian Markuszewski <adrian991999@wp.pl>
2020-09-04 16:00:10 +0200

    Add controllers and routes for server

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path =require('path')
-const cors = require('cors');
-const connectToDB = require('./db');
-
+const path = require("path");
+const cors = require("cors");
+const connectToDB = require("./db");
 
 //import dataBase
 connectToDB();
 
 //import routes
-const orderProductRoutes = require('./routes/order.routes');
-const productRoutes = require('./routes/product.routes');
+const orderProductRoutes = require("./routes/order.routes");
+const productRoutes = require("./routes/product.routes");
+const categoryRoutes = require("./routes/category.routes");
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/api',orderProductRoutes);
-app.use('/api',productRoutes);
+app.use("/api", orderProductRoutes);
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
 
-app.get('*',(req,res)=> {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-})
-
-app.listen(process.env.PORT || 8000, ()=> {
-    console.log("Server is running on port:8000");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-app.use((req,res)=> {
-    res.status(404).send("404 not found");
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running on port:8000");
+});
+app.use((req, res) => {
+  res.status(404).send("404 not found");
 });
